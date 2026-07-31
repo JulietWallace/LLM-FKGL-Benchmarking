430605dd123a0f6c1881dd3bb4661d403bd1ec32
Juliet Wallace <2760588w@student.gla.ac.uk>
2026-07-31 17:44:54 +0100

    added jupyter notebook for bert score

--- bert_score.py
+++ bert_score.py
@@ -1,4 +1,27 @@
 from bert_score import score
 import torch
 
+import pandas as pd
+
+import csv
+
+import glob
+
 #first get the data i want to use
+
+original_texts = pd.read_csv("./output/merged-fkgl-texts.csv")
+
+files = glob("./output/*_prompt_output.csv")
+
+data = pd.DataFrame()
+
+for file in files:
+    frame = pd.read_csv(file, header=None)
+    data = pd.concat([data, frame])
+
+
+
+
+
+
+
